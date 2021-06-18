60710499fee4e32584402f46f6102b06b06f5667
cergabr <78414909+cergabr@users.noreply.github.com>
2021-06-18 19:43:00 +0200

    add validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,51 +1,63 @@
 export function handleSubmit(event) {
     event.preventDefault();
-
-    // check what text was put into the form field
-    const formName = document.getElementById("name").value;
-    const inputURL = document.getElementById("url").value;
-    //VALIDATION
-    Client.checkForName(formName);
-    Client.urlValidate(inputURL);
     
     const formText = document.getElementById("text-fld").value;
     const formLang = document.getElementById("lang-fld").value.toLowerCase();
 
-    console.log("::: Form Submitted :::");
+    const txt={target:{id:"text-fld",value:formText}};
+    const lang={target:{id:"lang-fld",value:formLang}}
 
-    const formData = new FormData();
-    formData.append("key", "");
-    formData.append("txt", formText);
-    formData.append("lang", formLang);
+    if(Client.inputValidate(txt) && Client.inputValidate(lang)){    
 
-    const requestOption = {
-        method: "POST",
-        body: formData,
-        redirect: "follow"
-    }  
+        console.log("::: Form Submitted :::");
 
-    fetch("/apiKey")
-    .then(res => res.text())
-    .then(key => {
-        formData.set("key", key);
-        fetch("https://api.meaningcloud.com/sentiment-2.1", requestOption)
-        .then(res=>{
-            const data= res.json();
-            return data;
+        const formData = new FormData();
+        formData.append("key", "");
+        formData.append("txt", formText);
+        formData.append("lang", formLang);
+
+        const requestOption = {
+            method: "POST",
+            body: formData,
+            redirect: "follow"
+        }  
+
+        fetch("/apiKey")
+        .then(res => res.text())
+        .then(key => {
+            //formData.set("key", key);
+            fetch("https://api.meaningcloud.com/sentiment-2.1", requestOption)
+            .then(res=>{
+                const data= res.json();
+                return data;
+            })
+            .then(data => {
+                console.log(data)
+                const apiData = {
+                    sub: data.subjectivity.toLowerCase(),
+                    agreement: data.agreement.toLowerCase(),
+                    irony: data.irony.toLowerCase(),
+                    confidence: data.confidence
+                };
+                document.getElementById("subjectivity").innerHTML="Subjectivity: "+apiData.sub;
+                document.getElementById("agreement").innerHTML="Agreement: "+apiData.agreement;
+                document.getElementById("irony").innerHTML="Irony: "+apiData.irony;
+                document.getElementById("confidence").innerHTML="Analysis confidence: "+apiData.confidence+"%";
+
+                setTimeout(()=>{
+                    window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: 'smooth'});
+                },300)
+            })
         })
-        .then(data => {
-            console.log(data)
-            const apiData = {
-                sub: data.subjectivity.toLowerCase(),
-                agreement: data.agreement.toLowerCase(),
-                irony: data.irony.toLowerCase(),
-                confidence: data.confidence
-            };
-            document.getElementById("subjectivity").innerHTML="Subjectivity: "+apiData.sub;
-            document.getElementById("agreement").innerHTML="Agreement: "+apiData.agreement;
-            document.getElementById("irony").innerHTML="Irony: "+apiData.irony;
-            document.getElementById("confidence").innerHTML="Analysis confidence: "+apiData.confidence+"%";
-        })
-    })
-    .catch((error)=>{console.log("Error:",error);});
+        .catch((error)=>{console.log("Error:",error);});
+
+
+        for (let span of document.querySelectorAll("span")){
+            if (span.innerHTML!==""){
+                span.innerHTML="";
+                span.previousElementSibling.value="";
+                span.previousElementSibling.classList.remove("error");
+            }
+        }
+    }
 }
